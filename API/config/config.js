34d4a806b4b1a165b1a25dab1fdec57c182ab650
Daniel Alvares <app@ninja.co.za>
2016-03-21 16:19:18 +0000

    setup for heroku

--- API/config/config.js
+++ API/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: 5555,
+    PORT: process.env.PORT || 5555,
     SECRET: 'n1nj@_s3cr3t',
     DATABASE: 'mongodb://ninjamaster:N!njaSa1@ds035385.mlab.com:35385/ninja_app'
 }
