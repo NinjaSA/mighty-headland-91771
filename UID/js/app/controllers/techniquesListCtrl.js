4f89aa326cdc5d00fbe86b9871a3e21c228e23cb
Daniel Alvares <app@ninja.co.za>
2016-05-20 23:56:13 +0100

    Tweak pre-presentation

--- UID/js/app/controllers/techniquesListCtrl.js
+++ UID/js/app/controllers/techniquesListCtrl.js
@@ -1,5 +1,10 @@
 angular.module('ninjaApp')
-    .controller('techniquesListCtrl', ['$scope', '$stateParams', 'techniqueData', function($scope, $stateParams, techniqueData){
+    .controller('techniquesListCtrl', ['$scope', '$stateParams', '$timeout', 'techniqueData', function($scope, $stateParams, $timeout, techniqueData){
         $scope.techniques = techniqueData.techniques;
         $scope.label = $stateParams.label;
-}]);
+
+        $timeout(function(){
+            $scope.listItems = document.querySelectorAll('.list-group-item');
+            //$scope.$apply();
+        }, 0);
+}])
